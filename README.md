34626c0adc27c6fbaf9d1e0cf5e29a1a60de4e5a
BoolPurist <fnaumann@posteo.de>
2022-11-30 18:38:44 +0100

    Added progress with new library

--- README.md
+++ README.md
@@ -23,3 +23,29 @@ It cross compiles the app into for arm cpu as 64 bit and copies it to the raspbe
 Cross builds the app in a container with the needed compiler via podman. 
 Make sure the raspberry pie is on and can be accessed via ssh !
 
+
+### Current progress on cross compiling and control led matrix
+
+Cross compiling works right now.  However establishing a control to the led matrix fails.
+
+It fails with the error MatrixCreationError::ThreadTimedOut
+
+Problem: Harded coded 2 seconds for waiting until aborting the init process. 
+See line 303 under https://github.com/EmbersArc/rpi_led_panel/blob/main/src/rgb_matrix.rs
+
+Possible Solution: Increase waiting time. Need to change code in library.
+
+Switched from [rust-rpi-rgb-led-matrix](https://github.com/rust-rpi-led-matrix/rust-rpi-rgb-led-matrix)
+to [rpi_led_panel](https://github.com/EmbersArc/rpi_led_panel).
+
+Reason: rust-rpi-rgb-led-matrix currently does not support cross compiling. See following 
+[issue](https://github.com/rust-rpi-led-matrix/rust-rpi-rgb-led-matrix/issues/43)
+
+### Fix for errors panics
+
+If the programs panic with the error:
+GpioInitializationError::So: undModuleLoaded
+
+Sound on raspbe
+Then one need to change dtparam=audio to off, dtparam=audio=off, in the file /boot/config.txt
+
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,89 @@
-use rpi_led_matrix::{LedColor, LedMatrix, LedMatrixOptions, LedRuntimeOptions};
+use std::io::Write;
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::Rgb888,
+    prelude::*,
+    primitives::{Circle, Line, PrimitiveStyle},
+    text::{Alignment, Text},
+    Drawable,
+};
+use rpi_led_panel::{RGBMatrix, RGBMatrixConfig};
 
 fn main() {
-    let mut options = LedMatrixOptions::new();
-
-    options.set_hardware_mapping("adafruit-hat");
+    let config: RGBMatrixConfig = argh::from_env();
+    let rows = config.rows as i32;
+    let cols = config.cols as i32;
+    let options: RGBMatrixConfig = RGBMatrixConfig {
+        gpio_mapping: String::from("adafruit_hat"),
+        rows: 64,
+        cols: 64,
+        refresh_rate: 120,
+        pi_chip: None,
+        pwm_bits: 11,
+        pwm_lsb_nanoseconds: 130,
+        slowdown: None,
+        interlaced: false,
+        dither_bits: 0,
+        parallel: 1,
+        panel_type: None,
+        multiplexing: None,
+        row_setter: String::from("DirectRowAddressSetter"),
+    };
+    // Options from python version
+    // options.set_hardware_mapping("adafruit-hat");
     // options.set_rows(64);
     // options.set_cols(64);
-    // _ = options.set_brightness(100);
-    options.set_chain_length(4);
-    options.set_parallel(1);
-    // options.set_led_rgb_sequence("RGB");
-    // options.set_hardware_pulsing(false);
+    // options.set_brightness(100);
+    // options.set_chain_length(4);
+    // options.set_parallel(1);
+    // options.set_leg_rgb_sequence("RGB");
+    // options.set_hardware_pulsing(False);
 
-    match LedMatrix::new(Some(options), None) {
-        Ok(matrix) => {
-            let mut canvas = matrix.offscreen_canvas();
-            for red in 0..255 {
-                for green in 0..255 {
-                    for blue in 0..255 {
-                        canvas.fill(&LedColor { red, green, blue });
-                        canvas = matrix.swap(canvas);
-                    }
-                }
-            }
-        }
-        Err(error_message) => eprintln!("Could not create matrix:{}", error_message),
+    let result = RGBMatrix::new(options, 0);
+    let (mut matrix, mut canvas) = result.expect("Matrix initialization failed");
+    let circle = {
+        let thin_stroke = PrimitiveStyle::with_stroke(Rgb888::CSS_GRAY, 1);
+        Circle::with_center(
+            Point::new(rows / 2 - 1, cols / 2 - 1),
+            rows.min(cols) as u32 - 2,
+        )
+        .into_styled(thin_stroke)
     };
+    let top = Line::new(Point::new(0, 0), Point::new(cols - 1, 0))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::GREEN, 1));
+    let bottom = Line::new(Point::new(0, rows - 1), Point::new(cols - 1, rows - 1))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CYAN, 1));
+    let left = Line::new(Point::new(0, 0), Point::new(0, rows - 1))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::RED, 1));
+    let right = Line::new(Point::new(cols - 1, 0), Point::new(cols - 1, rows - 1))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::BLUE, 1));
+    let diagonal1 = Line::new(Point::new(0, 0), Point::new(cols - 1, rows - 1))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::YELLOW, 1));
+    let diagonal2 = Line::new(Point::new(cols - 1, 0), Point::new(0, rows - 1))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::MAGENTA, 1));
+
+    let text = Text::with_alignment(
+        "Hello\nWorld",
+        Point::new(cols / 2, rows / 2),
+        MonoTextStyle::new(&FONT_6X10, Rgb888::WHITE),
+        Alignment::Center,
+    );
+
+    for step in 0.. {
+        canvas.fill(0, 0, 0);
+        circle.draw(canvas.as_mut()).unwrap();
+        [diagonal1, diagonal2, top, bottom, left, right]
+            .iter()
+            .for_each(|line| line.draw(canvas.as_mut()).unwrap());
+        if (step / 100) % 2 == 0 {
+            text.draw(canvas.as_mut()).unwrap();
+        }
+        canvas = matrix.update_on_vsync(canvas);
+
+        if step % 120 == 0 {
+            print!("\r{:>100}\rFramerate: {}", "", matrix.get_framerate());
+            std::io::stdout().flush().unwrap();
+        }
+    }
 }
